442c7f78b654a671a8e8712f33686721a325768f
Jatin Kumar <Jatin.Kumar@asl.mum.sst>
2024-07-17 23:57:47 +0530

    Initial Commit

---
+++ Models/Professor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentCollege.Models
+{
+    internal class Professor:Person
+    {
+        public double Salary {  get; set; }
+        public Professor() { }
+
+        public Professor(int id, string address, string dob, double salary):base(id, address, dob)
+        {
+            Salary = salary;
+        }
+
+        public override string PrintDetails()
+        {
+            return $"{base.PrintDetails()}\nSalary:{Salary}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using StudentCollege.Models;
+
+namespace StudentCollege
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person(101,"Jammu","22/03/2002");
+            Professor professor= new Professor(1002,"Mumbai","13/09/1983",50000);
+            Student student = new Student(201,"Navi-Mumbai","12/05/2000","Comps");
+            ShowDetails(person);
+            ShowDetails(professor);
+            ShowDetails(student);
+        }
+
+        public static void ShowDetails(Person person)
+        {
+            Console.WriteLine($"===================={person.GetType().Name}=======================");
+            Console.WriteLine(person.PrintDetails());
+        }
+    }
+}
